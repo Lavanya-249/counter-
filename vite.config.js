a814c41ecd300f069c1df963f771eda9f5567a77
LAVANYA <yelisettilavanya@gmail.com>
2026-02-10 11:45:09 +0530

    changed files

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/counter/",
+  base:"/counter-/",
   plugins: [
     react({
       babel: {
